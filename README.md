ae3c1dc48237ae80ed2b12dd8a0096e531aa148d
keithching <keithching91@gmail.com>
2021-12-27 23:19:44 +0800

    incorporated firebase database support for simple CRUD

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-## The Odin Project  
+The Odin Project  
 ## Project: Library  
-### Practice: object, object constructor
+
+### Updates on 27 Dec 21
+* Refactored with Firebase Database support
+* For create, read, update, and delete
 
 ### Updates on 3 Nov 21
 * Incorporated simple input validations with JavaScript
 
 ### Updates on 7 Oct 21  
 * Refactor by using `class` instead of plain constructor
+
+### Practice
+* object, object constructor
--- src/index.js
+++ src/index.js
@@ -11,10 +11,13 @@ import {
     updateDoc,
     doc,
     serverTimestamp,
+    getDoc,
+    deleteDoc
 } from 'firebase/firestore';
 
 import './style.css';
 
+// initialize firebase
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyB6YQ81OmLCHM4pe3P9uI28pVJrmMAZZJc",
     authDomain: "library-8b24c.firebaseapp.com",
@@ -23,15 +26,143 @@ const firebaseApp = initializeApp({
     messagingSenderId: "1021396265744",
     appId: "1:1021396265744:web:564501778c7fc2c5f15395"
 });
+// get Firebase Database
 const db = getFirestore(firebaseApp);
 
 
-// an array of book objects
-let myLibrary = [];
+// // an array of book objects
+// let myLibrary = [];
 
-// adding a book to the myLibrary array
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+// // adding a book to the myLibrary array
+// function addBookToLibrary(book) {
+//     myLibrary.push(book);
+// }
+
+// save a book to Firebase Database
+async function saveBook(book) {
+    try {
+        await addDoc(collection(db, 'books'), {
+            author: book.author,
+            title: book.title,
+            pages: book.pages,
+            status: book.read ? 'read' : 'not read yet',
+            timestamp: serverTimestamp()
+        });
+    }
+    catch(error) {
+        console.log('Error writing new book to Firebase Database', error);
+    }
+}
+
+// loading the books from Firebase Database into the DOM
+function loadBooks() {
+    // query for loading books according to timestamps in descending order
+    const booksQuery = query(collection(db, 'books'), orderBy('timestamp', 'desc'));
+
+    // listening to the query
+    onSnapshot(booksQuery, function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
+            if (change.type === 'removed') {
+                // remove a book from the UI
+                removeBookFromUI(change.doc.id);
+            } else {
+                let book = change.doc.data();
+                // display book to the UI
+                displayBook(change.doc.id, book.timestamp, book.author, book.title,
+                            book.pages, book.status);
+            }
+        });
+    });
+}
+
+// create and insert book into DOM (Placeholder for a book's content to display)
+function createAndInsertBookToUI(id) {
+    const container = document.createElement('div');
+    container.classList.add('book-container');
+    container.setAttribute('id', id);
+    
+    // also need to generate some placeholders for book's content to feed into in the displayBook function call
+    // a skeleton
+    const author = document.createElement('div');
+    author.classList.add('author');
+
+    const title = document.createElement('div');
+    title.classList.add('title');
+
+    const pages = document.createElement('div');
+    pages.classList.add('pages');
+
+    const status = document.createElement('div');
+    status.classList.add('status');
+
+    container.appendChild(author);
+    container.appendChild(title);
+    container.appendChild(pages);
+    container.appendChild(status);
+
+    const updateStatusButton = document.createElement('button');
+    updateStatusButton.classList.add('updateBookStatus');
+    updateStatusButton.textContent = 'update read status';
+    updateStatusButton.addEventListener('click', updateBookReadStatus);
+    container.appendChild(updateStatusButton);
+
+    const removeBookButton = document.createElement('button');
+    removeBookButton.classList.add('removeBook');
+    removeBookButton.textContent = 'remove book';
+    removeBookButton.addEventListener('click', removeBookFromDatabase);
+    container.appendChild(removeBookButton);
+
+    document.querySelector('.container').appendChild(container);
+
+    return container;
+}
+
+// display a book in the UI
+function displayBook(id, timestamp, author, title, pages, status) {
+    let div = document.getElementById(id) || createAndInsertBookToUI(id);
+
+    div.querySelector('.author').textContent = `author: ${author}`;
+    div.querySelector('.title').textContent = `title: ${title}`;
+    div.querySelector('.pages').textContent = `pages: ${pages}`;
+    div.querySelector('.status').textContent = `status: ${status}`;
+}
+
+// update the status of book via setDoc to Firebase Database
+// callback
+async function updateBookReadStatus(e) {
+    const bookID = e.target.parentNode.id;
+    const book = doc(db, 'books', bookID);
+    const docSnap = await getDoc(book);
+
+    if (docSnap.exists()) {
+        const data = docSnap.data();
+
+        if (data.status === 'read') {
+            await updateDoc(book, {
+                    status: 'not read yet'
+                });
+        } else if (data.status === 'not read yet') {
+            await updateDoc(book, {
+                status: 'read'
+            });
+        }
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}
+
+// remove the book from Firebase Database
+// callback
+async function removeBookFromDatabase(e) {
+    const bookID = e.target.parentNode.id;
+
+    await deleteDoc(doc(db, 'books', bookID));
+}
+
+// remove a book from the DOM
+function removeBookFromUI(id) {
+    document.getElementById(id).remove();
 }
 
 
@@ -113,11 +244,9 @@ submitBtn.addEventListener('click', () => {
         readnotreadFieldError.textContent = '';
     }
 
-
     const author = authorField.value;
     const title = titleField.value;
     const pages = pagesField.value;
-
 
     let read = false;
     if (readField.checked) {
@@ -127,99 +256,43 @@ submitBtn.addEventListener('click', () => {
     }
 
     const book = new Book(author, title, pages, read);
-    addBookToLibrary(book);
-    refreshDisplay();
+    saveBook(book); // to firebase database
 
     document.querySelector('#author').value = null;
     document.querySelector('#title').value = null;
     document.querySelector('#pages').value = null;
     document.getElementById('read').checked = false;
     document.getElementById('notread').checked = false;
-
 });
 
 function showError() {
-
     // check for author field
     if (!authorField.validity.valid) {
-
         if (authorField.validity.valueMissing) {
             authorFieldError.textContent = 'author name needed';
         }
-
     }
 
     // check for title field
     if (!titleField.validity.valid) {
-
         if (titleField.validity.valueMissing) {
             titleFieldError.textContent = 'title name needed';
         }
-
     }
 
     // check for pages field
     if (!pagesField.validity.valid) {
-
         if (pagesField.validity.valueMissing) {
             pagesFieldError.textContent = 'pages needed';
         }
-
     }
 
     // check for read field
     if (!readField.checked && !notreadField.checked) {
-
         readnotreadFieldError.textContent = 'pls check either box';
-
     }
-
-
 }
 
-
-// function that loops through the array and displays each book on the page
-function refreshDisplay() {
-
-    const container = document.querySelector('.container');
-
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-
-    for (let i = 0; i < myLibrary.length; i++) {
-
-        const bookCard = document.createElement('div');
-        bookCard.setAttribute('class','bookCard');
-        // data-attribute to DOM element to correlate with myLibrary array
-        bookCard.setAttribute('data-index', i);
-
-        const authorDisplay = document.createElement('p');
-        authorDisplay.setAttribute('class', 'author');
-        authorDisplay.textContent = `Author: ${myLibrary[i].author}`; // TODO
-
-        const titleDisplay = document.createElement('p');
-        titleDisplay.setAttribute('class', 'title');
-        titleDisplay.textContent = `Title: ${myLibrary[i].title}`;
-
-        const pagesDisplay = document.createElement('p');
-        pagesDisplay.setAttribute('class', 'pages');
-        pagesDisplay.textContent = `Number of Pages: ${myLibrary[i].pages}`;
-
-        const readDisplay = document.createElement('p');
-        readDisplay.setAttribute('class', 'status');
-        readDisplay.textContent = `Status: ${myLibrary[i].status()}`; 
-
-        bookCard.appendChild(authorDisplay);
-        bookCard.appendChild(titleDisplay);
-        bookCard.appendChild(pagesDisplay);
-        bookCard.appendChild(readDisplay);
-        container.appendChild(bookCard);
-    }
-
-    addRemoveButton();
-    addChangeStatusButton();
-}
 
 // "NEW BOOK" button that brings up a form for user to input the new book's details
 // e.g. author, title, number of pages, whether it's been read, ...
@@ -234,53 +307,6 @@ function displayForm() {
 const newBook = document.querySelector('#newBook');
 newBook.addEventListener('click', displayForm);
 
-// add a remove button on each book's display to remove the book from the library
-function addRemoveButton() {
-    const cards = document.querySelectorAll('.bookCard');
-    cards.forEach(card => {
-        const button = document.createElement('button');
-        button.setAttribute('class', 'remove');
-        button.textContent = 'Remove Book';
-        button.addEventListener('click', removeBook);
-        card.appendChild(button);
-    });    
-}
-
-function removeBook() {
-    const bookCard = this.closest('.bookCard'); // get the closest bookCard element
-    // remove book from myLibrary array
-    const index = bookCard.getAttribute('data-index');
-
-    myLibrary.splice(index, 1);
-    refreshDisplay();
-}
-
-
-// add a button on each book's display to change its read status
-function addChangeStatusButton() {
-    const cards = document.querySelectorAll('.bookCard');
-    cards.forEach(card => {
-        const button = document.createElement('button');
-        button.setAttribute('class', 'changestatus');
-        button.textContent = 'Update Read Status';
-        button.addEventListener('click', changeBookStatus);
-        card.appendChild(button);
-    });        
-}
-
-function changeBookStatus() {
-    const bookCard = this.closest('.bookCard'); // get the closest bookCard element
-    // remove book from myLibrary array
-    const index = bookCard.getAttribute('data-index');
-
-    if (myLibrary[index].read) {
-        myLibrary[index].read = false;
-    } else {
-        myLibrary[index].read = true;
-    }
-
-    refreshDisplay();
-}
 
 // cancel button to clear the form values and hide the form
 function cancelForm() {
@@ -303,3 +329,8 @@ function cancelForm() {
 const cancelBtn = document.querySelector('#cancel');
 cancelBtn.addEventListener('click', cancelForm);
 
+
+
+
+// load books into the app
+loadBooks();
